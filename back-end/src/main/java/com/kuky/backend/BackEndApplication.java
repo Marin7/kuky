e8c641862c54b78c3936681ebc7600ecfd4960bb
Marin Petcu <marin.petcu33@gmail.com>
2026-06-07 22:25:25 +0300

    Add back-end skeleton

---
+++ back-end/src/main/java/com/kuky/backend/BackEndApplication.java
@@ -0,0 +1,12 @@
+package com.kuky.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+}
---
+++ back-end/src/test/java/com/kuky/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuky.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
